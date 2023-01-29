656cf59dca62304ebbe91293bfab23c5acdc43f2
Vitalii Masliuhov <masliks456@gmail.com>
2023-01-30 01:06:49 +0200

    v1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,57 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery")
+
+galleryEl.insertAdjacentHTML('beforeEnd', createItemsEl(galleryItems))
+
+function createItemsEl(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>`
+    }).join('');
+}
+
+
+// const createItemsEl = galleryItems.map(({ preview, original, description }) => {
+//     return  `
+//     <div class="gallery__item">
+//      <a class="gallery__link" href="${original}">
+//          <img
+//            class="gallery__image"
+//            src="${preview}"
+//            data-source="${original}"
+//          alt="${description}"
+//            />
+//       </a>
+//     </div>`
+// }).join('')
+
+
+
+galleryEl.addEventListener('click', onGalleryClick)
+
+
+function onGalleryClick(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== 'IMG') {
+        rexturn;
+    }
+
+    basicLightbox.create(`<img src="${e.target.dataset.source}">`).show()  
+
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery")
+
+galleryEl.insertAdjacentHTML('beforeEnd', createItemsEl(galleryItems))
+
+function createItemsEl(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    overlay: false,
+    captions: true,
+    captionType: 'attr',
+    captionsData    : 'alt',
+    captionPosition: 'outside',
+    captionDelay: 250,
+})
